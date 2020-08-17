cca7cd50645c632e73785d9fbc0a3bfbdf3d5319
GorginZ <georgia.z.leng@gmail.com>
2020-08-18 09:48:33 +1000

    step 4

--- Program.cs
+++ Program.cs
@@ -6,22 +6,21 @@ namespace StringCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(StringCalc("2,4,2,1,5,6"));
+            Console.WriteLine("Welcome to step 4 calculator enter some numers eg: 2,5,2,1,3");
+            var numbers = Console.ReadLine();
+            Console.WriteLine(StringCalc(numbers));
         }
         public static string StringCalc(string numbers)
         {
             var newNums = numbers.Split(",");
             var res = 0;
             int count = 0;
-
+            //refactor to not use loop?
             foreach (string element in newNums)
             {
                 count++;
                 int workingNum = Int32.Parse(element);
                 res += workingNum;
-              // Console.WriteLine(res);
-               
             }
             
             return res.ToString();
