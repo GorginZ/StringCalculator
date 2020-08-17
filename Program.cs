c347efc4822a0460e8bc6ba074c99ed9beb78fa0
GorginZ <georgia.z.leng@gmail.com>
2020-08-18 09:34:27 +1000

    string calculator, uses loop, will refactor

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StringCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(StringCalc("2,4,2,1,5,6"));
+        }
+        public static string StringCalc(string numbers)
+        {
+            var newNums = numbers.Split(",");
+            var res = 0;
+            int count = 0;
+
+            foreach (string element in newNums)
+            {
+                count++;
+                int workingNum = Int32.Parse(element);
+                res += workingNum;
+              // Console.WriteLine(res);
+               
+            }
+            
+            return res.ToString();
+        }
+    }
+}
