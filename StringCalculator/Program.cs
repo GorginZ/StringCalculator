23de430b5d27a08105b9602a18d91f41513b3f73
GorginZ <georgia.z.leng@gmail.com>
2020-08-20 09:30:19 +1000

    added new logic in for each loop to tryparse int, if empty char slips through the regex, this is no longer a fail point, it will continue to next value, ideally will fix the regex but this error catching logic is probably practical and ideal anyay

--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -1,35 +1,41 @@
 ﻿using System;
 using System.Text.RegularExpressions;
-using System.Collections.Generic; 
+using System.Collections.Generic;
 
 namespace StringCalculator
 {
-    public class Program
+  public class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to step 4 calculator enter some numers eg: 2,5,2,1,3");
-            var numbers = Console.ReadLine();
-            Console.WriteLine(StringCalc(numbers));
-        }
-        public static string StringCalc(string numbers)
-        {   
-            // var newString = Regex.Replace(numbers, @"[^0-9]",""); 
-            
-            // @"[,;\n;]"
-             string[] newNums = Regex.Split(numbers, @"[^0-9]+");
-             var newString = newNums.ToString();
-             newString = Regex.Replace(newString, @"[^0-9]", "");
-             string[] workingNums = newString.Split();
-             
-            var res = 0;
-            foreach (string element in workingNums)
-            {
-                int workingNum = Int32.Parse(element);
-                res += workingNum;
-            }
-            
-            return res.ToString();
-        }
+      Console.WriteLine("Welcome to step 4 calculator enter some numers eg: 2,5,2,1,3");
+      var numbers = Console.ReadLine();
+      Console.WriteLine(StringCalc(numbers));
     }
+    public static string StringCalc(string numbers)
+    {
+
+      string[] newNums = Regex.Split(numbers, @"[^0-9]+");
+
+
+      var res = 0;
+      foreach (string element in newNums)
+      {
+        int workingNum;
+        bool resBool;
+        resBool = int.TryParse(element, out workingNum);
+        if (resBool == true)
+        {
+          res += workingNum;
+        }
+        if (resBool == false)
+        {
+          continue;
+        }
+
+      }
+
+      return res.ToString();
+    }
+  }
 }
