7667c220c5e9f419f76d85e24d112f4fe308e199
GorginZ <georgia.z.leng@gmail.com>
2020-08-20 17:03:19 +1000

    added tests for steps 2 and 3. Test 12 written

--- StringCalculator.Tests/StringCalculatorTests.cs
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -6,8 +6,18 @@ namespace StringCalculator.UnitTests
 {
   public class StringCalculatorTests
   {
+    
     [Fact]
-    public void WithSingleStringArgumentsReturnsThatDigit()
+    public void Step1SingleInputSingleOutput()
+    {
+      //Arrange
+      var result = Program.Add("");
+  //"expected", actual
+      Assert.Equal("0", result);
+
+    }
+[Fact]
+    public void Step2SingleNumberSingleNumberOutputt()
     {
       //Arrange
       var result = Program.Add("3");
@@ -16,42 +26,73 @@ namespace StringCalculator.UnitTests
 
     }
     [Fact]
-    public void WithMultipleDigitStringInputReturnsSum()
+    public void Step3TwoNumbersReturnSumOfTwoNumbers()
+    {
+      //Arrange
+      var result = Program.Add("1,2");
+  //"expected", actual
+      Assert.Equal("3", result);
+
+    }
+
+    [Fact]
+    public void Step4WithMultipleDigitStringInputReturnsSum()
     {
       var result = Program.Add("4,5,6,4,3");
       Assert.Equal("22", result);
     }
     [Fact]
-    public void NewLineOrCommarSeperatorShouldReturnSum()
+    public void Step5NewLineOrCommarSeperatorShouldReturnSum()
     {
       var result = Program.Add("1,2\n3");
       Assert.Equal("6", result);
     }
     [Fact]
-    public void SupportsOtherDelimiters()
+    public void Step6SupportsOtherDelimiters()
 
     {
       var result = Program.Add("//;\n1;2");
       Assert.Equal("3", result);
     }
     [Fact]
-    public void CallingWithaNegativeShouldThrowException()
+    public void Step7CallingWithaNegativeShouldThrowException()
 
     {
       Assert.Throws(typeof(Exception), () => Program.Add("-1,2,-3"));
     }
     [Fact]
-    public void IgnoresNumbersGreaterThan1000()
+    public void Step8IgnoresNumbersGreaterThan1000()
 
     {
       var result = Program.Add("1000,1001,2");
       Assert.Equal("2",result);
     }
- [Fact]
-    public void Step9DelimitersAnyLength()
+     [Fact]
+    public void Step9DelimitersCanBeOfAnyLengthWFollowingFormat()
 
     {
       var result = Program.Add("//[***]\n1***2***3");
+      Assert.Equal("6",result);
+    }
+ [Fact]
+    public void Step10MultipleDelimiters()
+
+    {
+      var result = Program.Add("//[*][%]\n1*2%3");
+      Assert.Equal("6",result);
+    }
+     [Fact]
+    public void Step11HandleMultipleDelimitersWithLengthLongerThanOneChar()
+
+    {
+      var result = Program.Add("//[***][#][%]\n1***2#3%4");
+      Assert.Equal("10",result);
+    }
+     [Fact]
+    public void Step12HandleDelimitersThatHaveNumbersAsPartOfThem()
+  // Handle delimiters that have numbers as part of them, where the number cannot be on the edge of a delimiter.
+    {
+      var result = Program.Add("//[*1*][%]\n1*1*2%3");
       Assert.Equal("6",result);
     }
 
